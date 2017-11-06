bc9537eba43c669d0f0d27ed01a5c5dbcfcb6c4c
CAUCHOIS Guillaume <guillaume.cauchois@epitech.eu>
2017-11-06 18:25:31 +0100

    delete(gameCards) : useless elements in server networking
    Delete useless networking elements (chat)

--- Server/ServerHandler.cs
+++ Server/ServerHandler.cs
@@ -8,7 +8,10 @@
     public class ServerHandler : SimpleChannelInboundHandler<string>
     {
         static volatile IChannelGroup group;
-
+        
+        /**
+         * Handle the connection
+         */
         public override void ChannelActive(IChannelHandlerContext contex)
         {
             IChannelGroup g = group;
@@ -37,28 +40,22 @@
                 this.id = id;
             }
 
-            public bool Matches(IChannel channel) => channel.Id != this.id;
+            public bool Matches(IChannel channel) => !channel.Id.Equals(this.id);
         }
 
         protected override void ChannelRead0(IChannelHandlerContext contex, string msg)
         {
-            //send message to all but this one
-            string broadcast = string.Format("[{0}] {1}\n", contex.Channel.RemoteAddress, msg);
-            string response = string.Format("[you] {0}\n", msg);
+            string broadcast    = string.Format("[{0}] {1}\n", contex.Channel.RemoteAddress, msg);
+            string response     = string.Format("[you] {0}\n", msg);
             group.WriteAndFlushAsync(broadcast, new EveryOneBut(contex.Channel.Id));
             contex.WriteAndFlushAsync(response);
-
-            if (string.Equals("bye", msg, StringComparison.OrdinalIgnoreCase))
-            {
-                contex.CloseAsync();
-            }
         }
 
         public override void ChannelReadComplete(IChannelHandlerContext ctx) => ctx.Flush();
 
         public override void ExceptionCaught(IChannelHandlerContext ctx, Exception e)
         {
-            Console.WriteLine("{0}", e.StackTrace);
+            Console.Error.WriteLine("{0}", e.StackTrace);
             ctx.CloseAsync();
         }
 
