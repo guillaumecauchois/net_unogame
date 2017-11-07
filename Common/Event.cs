f85dd43fcfef38db18e93ba41099eed4c2fe6040
CAUCHOIS Guillaume <guillaume.cauchois@epitech.eu>
2017-11-07 17:27:45 +0100

    Update(Common): Implement modification from UML
    Implementation of modifications made in the UML
    Event /  TurnResponse

---
+++ Common/Event.cs
@@ -0,0 +1,55 @@
+﻿namespace Common
+{
+    public enum EventType
+    {
+        InvalidCommand = -1,
+        YouTurn = 0,
+        PlayerTurn,
+        PlayerHasPlayed,
+        EndGame,
+    }
+    
+    public class Event
+    {
+        private Table        _table;
+        private Player       _player;
+        private EventType    _type;
+
+        public Event(EventType type, Table table, Player player)
+        {
+            _player = player;
+            _table = table;
+            _type = type;
+        }
+
+        public void SetTable(Table table)
+        {
+            _table = table;
+        }
+
+        public Table GetTable()
+        {
+            return _table;
+        }
+
+        public void SetPlayer(Player player)
+        {
+            _player = player;
+        }
+
+        public Player GetPlayer()
+        {
+            return _player;
+        }
+
+        public void SetType(EventType type)
+        {
+            _type = type;
+        }
+
+        public EventType GetEventType()
+        {
+            return _type;
+        }
+    }
+}
---
+++ Common/TurnResponse.cs
@@ -0,0 +1,40 @@
+﻿namespace Common
+{
+    public class TurnResponse
+    {
+        private Card _card;
+        private bool _uno;
+
+        public TurnResponse()
+        {
+            _card = null;
+            _uno = false;
+        }
+
+        public TurnResponse(Card card, bool uno)
+        {
+            _card = card;
+            _uno = uno;
+        }
+
+        public void Uno()
+        {
+            _uno = true;
+        }
+
+        public void SetCard(Card card)
+        {
+            _card = card;
+        }
+
+        public Card GetCard()
+        {
+            return _card;
+        }
+        
+        public bool HasUno()
+        {
+            return (_uno);
+        }
+    }
+}
