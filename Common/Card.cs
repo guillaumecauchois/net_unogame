960496f0f0d5f9bbbe6065ee04c8e4636523f7b0
stasza_p <pierre.staszak@epitech.eu>
2017-11-07 16:05:40 +0100

    CardValue changed

--- Common/Card.cs
+++ Common/Card.cs
@@ -6,12 +6,14 @@
         Undefined,
         Green,
         Red,
-        Yellow
+        Yellow,
+        Blue
     }
 
     public enum CardValue
     {
         Undefined,
+        Zero,
         One,
         Two,
         Three,
@@ -22,14 +24,14 @@
         Eight,
         Nine,
         Plus2,
-        Plus4,
         Revert,
         PassTurn,
-        ChangeLog
+        ChangeColor,
+        Plus4
     }
     
     /* Class Card */
-    public abstract class Card
+    public class Card
     {
         /* Attributs */
         private CardColor _color;
---
+++ Server/CardFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Dynamic;
+using Common;
+
+namespace Server
+{
+    public class CardFactory
+    {
+        private ArrayList _cards;
+
+        public CardFactory()
+        {
+            _cards = new ArrayList();
+        }
+
+        public void GenerateDeck()
+        {
+            for (int i = 1; i < (int) Common.CardColor.Blue + 1; i++)
+            {
+                _cards.Add(new Common.Card((Common.CardColor)i, Common.CardValue.Zero));
+                for (int y = 2; y < (int) Common.CardValue.ChangeColor; y++)
+                {
+                    _cards.Add(new Common.Card((Common.CardColor)i, (Common.CardValue)y));  
+                    _cards.Add(new Common.Card((Common.CardColor)i, (Common.CardValue)y));
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                _cards.Add(new Common.Card(Common.CardColor.Undefined, Common.CardValue.Plus4));
+                _cards.Add(new Common.Card(Common.CardColor.Undefined, Common.CardValue.ChangeColor));
+            }
+        }
+    }
+}
