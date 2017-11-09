3e7d1dd8f654e7134578ff341e12bed0b0219691
CAUCHOIS Guillaume <guillaume.cauchois@epitech.eu>
2017-11-09 05:00:03 +0100

    feat(Common.Network) :  Serialization
    Add a class that handle all Serialization / Deserialization I/O

---
+++ Common/SerializeHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Common
+{
+    public static class SerializeHandler
+    {
+        public static string SerializeObj<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(ms, obj);
+                return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+            }
+        }
+
+        public static T DeserializeObject<T>(string serializedObj)
+        {
+            var arr = Convert.FromBase64String(serializedObj);
+            using (var ms = new MemoryStream(arr))
+            {
+                var obj = ProtoBuf.Serializer.Deserialize<T>(ms);
+                return obj;
+            }
+        }
+    }
+}
